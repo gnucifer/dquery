b6247c681530fb035a37bacc36a280eb39cd8308
David Gustafsson <david.gustafsson@xelera.se>
2012-11-12 18:59:21 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DQuery',
-    version='0.1.0-alpha',
+    version='0.1.0-beta',
     author='David Gustafsson',
     author_email='david.gustafsson@xelera.se',
     packages=find_packages(),
